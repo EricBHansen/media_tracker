853836f35cadb63d43764237e42c789dc3ba18b8
Tesla <tesla.a.lyon@gmail.com>
2024-08-31 18:05:46 -0700

    Update server.py

--- group_project/media_tracker/server.py
+++ group_project/media_tracker/server.py
@@ -1,5 +1,5 @@
 from flask_app import app
-from flask_app.controllers import users
+from flask_app.controllers import users, movies
 
 # need to import: movies, favorites from flask_app.controllers
 
